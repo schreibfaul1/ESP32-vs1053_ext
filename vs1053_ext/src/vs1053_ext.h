13a98ad4c3adff7cbad5ec2a74b0ccde1e033897
Wolle <schreibfaul1@t-online.de>
2018-03-09 22:51:42 +0100

    Update vs1053_ext.h

--- vs1053_ext/src/vs1053_ext.h
+++ vs1053_ext/src/vs1053_ext.h
@@ -28,10 +28,10 @@ extern __attribute__((weak)) void vs1053_bitrate(const char*);
 class VS1053
 {
   private:
-    WiFiClient client;                  			// An instance of the client
-	File mp3file;
+    WiFiClient client;                                  // instance of the client
+    File mp3file;
   private:
-	uint8_t       cs_pin ;                        	// Pin where CS line is connected
+    uint8_t       cs_pin ;                        	// Pin where CS line is connected
     uint8_t       dcs_pin ;                       	// Pin where DCS line is connected
     uint8_t       dreq_pin ;                      	// Pin where DREQ line is connected
     uint8_t       curvol ;                        	// Current volume setting 0..100%
@@ -76,59 +76,59 @@ class VS1053
 
     uint32_t        m_t0;                           // Keep alive, end a playlist
     uint8_t         m_endFillByte ;                 // Byte to send when stopping song
-	uint16_t		m_datamode=0;                   // Statemaschine
-	String          m_metaline ;                    // Readable line in metadata
-	String          m_mp3title;                     // Name of the mp3 file
-	bool            m_chunked = false ;             // Station provides chunked transfer
+    uint16_t        m_datamode=0;                   // Statemaschine
+    String          m_metaline ;                    // Readable line in metadata
+    String          m_mp3title;                     // Name of the mp3 file
+    bool            m_chunked = false ;             // Station provides chunked transfer
     bool            m_ctseen=false;                 // First line of header seen or not
     bool            m_firstchunk=true;              // First chunk as input
     int             m_LFcount;                      // Detection of end of header
     int             m_chunkcount = 0 ;              // Counter for chunked transfer
-	int             m_metaint = 0;                  // Number of databytes between metadata
-	int             m_bitrate = 0;                  // Bitrate in kb/sec
-	int16_t         m_btp=0;                        // Bytes to play
-	uint32_t        m_totalcount = 0;            	// Counter mp3 data
-	int             m_metacount=0;                  // Number of bytes in metadata
-	String          m_icyname ;                     // Icecast station name
-	String          m_icystreamtitle ;              // Streamtitle from metadata
-	String          m_playlist ;                    // The URL of the specified playlist
-	int8_t          m_playlist_num = 0 ;            // Nonzero for selection from playlist
+    int             m_metaint = 0;                  // Number of databytes between metadata
+    int             m_bitrate = 0;                  // Bitrate in kb/sec
+    int16_t         m_btp=0;                        // Bytes to play
+    uint32_t        m_totalcount = 0;               // Counter mp3 data
+    int             m_metacount=0;                  // Number of bytes in metadata
+    String          m_icyname ;                     // Icecast station name
+    String          m_icystreamtitle ;              // Streamtitle from metadata
+    String          m_playlist ;                    // The URL of the specified playlist
+    int8_t          m_playlist_num = 0 ;            // Nonzero for selection from playlist
     boolean         m_firstmetabyte=false;          // True if first metabyte (counter)
-	boolean         m_f_hostreq = false ;           // Request for new host
-	boolean         m_f_localfile = false ;         // Play from local mp3-file
+    boolean         m_f_hostreq = false ;           // Request for new host
+    boolean         m_f_localfile = false ;         // Play from local mp3-file
     boolean         m_f_webstream = false ;         // Play from URL
-	boolean         m_f_plsFile=false;              // Set if URL is known
-	boolean         m_f_plsTitle=false;             // Set if StationName is knowm
-	boolean         m_f_ogg=false;                  // Set if oggstream
-	String          m_plsURL;
-	String          m_plsStationName;
-	const char volumetable[22]={   0,50,60,65,70,75,80,82,84,86,
-	    						  88,90,91,92,93,94,95,96,97,98,99,100}; //22 elements
+    boolean         m_f_plsFile=false;              // Set if URL is known
+    boolean         m_f_plsTitle=false;             // Set if StationName is knowm
+    boolean         m_f_ogg=false;                  // Set if oggstream
+    String          m_plsURL;
+    String          m_plsStationName;
+    const char volumetable[22]={   0,50,60,65,70,75,80,82,84,86,
+                                  88,90,91,92,93,94,95,96,97,98,99,100}; //22 elements
   protected:
-	inline void DCS_HIGH() {GPIO.out_w1ts = (1 << dcs_pin);}
-	inline void DCS_LOW()  {GPIO.out_w1tc = (1 << dcs_pin);}
-	inline void CS_HIGH()  {GPIO.out_w1ts = (1 << cs_pin);}
-	inline void CS_LOW()   {GPIO.out_w1tc = (1 << cs_pin);}
-	inline void await_data_request() const
+    inline void DCS_HIGH() {GPIO.out_w1ts = (1 << dcs_pin);}
+    inline void DCS_LOW()  {GPIO.out_w1tc = (1 << dcs_pin);}
+    inline void CS_HIGH()  {GPIO.out_w1ts = (1 << cs_pin);}
+    inline void CS_LOW()   {GPIO.out_w1tc = (1 << cs_pin);}
+    inline void await_data_request() const
     {
       while ( !digitalRead ( dreq_pin ) )
       {
         NOP() ;                                   	// Very short delay
       }
     }
-	void control_mode_on();
-	void control_mode_off();
-	void data_mode_on();
-	void data_mode_off();
+    void control_mode_on();
+    void control_mode_off();
+    void data_mode_on();
+    void data_mode_off();
     uint16_t read_register ( uint8_t _reg ) ;
     void     write_register ( uint8_t _reg, uint16_t _value );
     void     sdi_send_buffer ( uint8_t* data, size_t len ) ;
     void     sdi_send_fillers ( size_t length ) ;
     void     wram_write ( uint16_t address, uint16_t data ) ;
     uint16_t wram_read ( uint16_t address ) ;
-    void 	 handlebyte(uint8_t b);
-    void 	 showstreamtitle ( const char *ml, bool full );
-    bool 	 chkhdrline ( const char* str );
+    void     handlebyte(uint8_t b);
+    void     showstreamtitle ( const char *ml, bool full );
+    bool     chkhdrline ( const char* str );
     void     startSong() ;                               // Prepare to start playing. Call this each
                                                          // time a new song starts.
     void     stopSong() ;                                // Finish playing a song. Call this after
